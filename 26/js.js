3511331d11703afc47174aa1918a7ce9a9466daf
Kaue-Brito099 <125032287+Kaue-Brito099@users.noreply.github.com>
2024-06-14 11:45:03 -0300

    Add files via upload

---
+++ 26/js.js
@@ -0,0 +1,24 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
